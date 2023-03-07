f6c8dc327c4c06868cd612994fc6258a53605357
minkere-kor <imminkyu@hotmail.com>
2023-03-06 21:22:45 -0800

    Plot and table for question #2

--- group-project/app.R
+++ group-project/app.R
@@ -14,6 +14,11 @@ library(rsconnect)
 
 absent <- read_delim("absenteeism.csv")
 
+total_abs <- absent %>% 
+  group_by(ID) %>% 
+  count(Reason_for_absence) %>% 
+  summarize(total_absences = sum(n)) 
+
 ui <- fluidPage(
 
     titlePanel("Absenteeism in the Workplace"),
@@ -51,8 +56,18 @@ ui <- fluidPage(
       
       tabPanel("Question 2",
                sidebarLayout(
-                 sidebarPanel(),
-                 mainPanel()
+                 sidebarPanel(
+                   p("text"),
+                   
+                   radioButtons("plotSelect",
+                                "Select Plot",
+                                choices = c("Education", "# of Children", "# of Pets"))
+                   
+                 ),
+                 mainPanel(
+                   plotOutput("socPlot"),
+                   dataTableOutput("socTable")
+                 )
                )),
       
       tabPanel("Question 3",
@@ -70,8 +85,107 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+## QUESTION 1
 
-
+## QUESTION 2
+  
+  output$socPlot <- renderPlot({
+    if(input$plotSelect == "Education"){
+      currentTable <- absent %>% 
+        select(ID, Education) %>% 
+        distinct(ID, Education) %>% 
+        merge(total_abs, by = "ID")
+      
+      currentPlot <- currentTable %>% 
+        ggplot(aes(Education,
+                   total_absences,
+                   fill=factor(ID))) +
+        geom_col(col="black",
+                 width = 0.5) +
+        labs(title = "Number of Absences based on Education",
+             x = "Education Level",
+             y = "Number of Absences",
+             fill = "Employee ID") +
+        theme(plot.title = element_text(size = 25),
+              axis.text = element_text(size = 10),
+              axis.title = element_text(size = 20),
+              legend.title = element_text(size = 15))
+    } 
+    
+    else if(input$plotSelect == "# of Children"){
+      currentTable <- absent %>% 
+        select(ID, Son) %>% 
+        distinct(ID, Son) %>% 
+        merge(total_abs, by = "ID")
+      
+      currentPlot <- currentTable %>% 
+        ggplot(aes(Son,
+                   total_absences,
+                   fill=factor(ID))) +
+        geom_col(col="black",
+                 width = 0.5) +
+        labs(title = "Number of Absences based on Number of Children",
+             x = "Number of Children",
+             y = "Number of Absences",
+             fill = "Employee ID") +
+        theme(plot.title = element_text(size = 25),
+              axis.text = element_text(size = 10),
+              axis.title = element_text(size = 20),
+              legend.title = element_text(size = 15))
+    }  
+    
+    else if(input$plotSelect == "# of Pets"){
+      currentTable <- absent %>% 
+        select(ID, Pet) %>% 
+        distinct(ID, Pet) %>% 
+        merge(total_abs, by = "ID")
+      
+      currentPlot <- currentTable %>% 
+        ggplot(aes(Pet,
+                   total_absences,
+                   fill=factor(ID))) +
+        geom_col(col="black",
+                 width = 0.5) +
+        labs(title = "Number of Absences based on Number of Pets",
+             x = "Number of Pets",
+             y = "Number of Absences",
+             fill = "Employee ID") +
+        theme(plot.title = element_text(size = 25),
+              axis.text = element_text(size = 10),
+              axis.title = element_text(size = 20),
+              legend.title = element_text(size = 15))
+    }
+    currentPlot
+  })
+  
+  output$socTable <- renderDataTable({
+    if(input$plotSelect == "Education"){
+      currentTable <- absent %>% 
+        select(ID, Education) %>% 
+        distinct(ID, Education) %>% 
+        merge(total_abs, by = "ID")
+    } 
+    
+    else if(input$plotSelect == "# of Children"){
+      currentTable <- absent %>% 
+        select(ID, Son) %>% 
+        distinct(ID, Son) %>% 
+        merge(total_abs, by = "ID")
+    }
+    
+    else if(input$plotSelect == "# of Pets"){
+      currentTable <- absent %>% 
+        select(ID, Pet) %>% 
+        distinct(ID, Pet) %>% 
+        merge(total_abs, by = "ID")
+    }
+    
+      currentTable %>% 
+        arrange(desc(total_absences)) %>% 
+        head(10)
+  })
+  
+## QUESTION 3
 }
 
 shinyApp(ui = ui, server = server)
